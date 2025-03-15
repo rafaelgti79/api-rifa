39fe12e567fe34a953a01847c851b1527d3b7460
rafaelgti79 <rafaelgti79@gmail.com>
2025-03-15 07:13:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import router from "./routes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Authorization',
+  }));
 app.use(router);
 
 app.listen(3001, () => {

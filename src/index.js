e0da38399af9d313d668b37d2bab7327a9835a81
rafaelgti79 <rafaelgti79@gmail.com>
2025-03-17 21:57:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ app.use(cors({
   }));
 app.use(router);
 
+ app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(3001, () => {
     console.log("Servidor rodando na porta: 3001..");
 });
